60daf259eca891f916b4c6de0ab350b95485965e
Leandro França <leandro.franca@valtech.com>
2019-01-21 18:33:45 -0200

    testestestea

--- offline.js
+++ offline.js
@@ -5,7 +5,7 @@
 
 	const staticCacheName = 'cache_calculadoras';	
 	
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
