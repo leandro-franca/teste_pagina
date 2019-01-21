d8ebcc5b5917c632d14918fa824d3190e25eb107
Leandro França <leandro.franca@valtech.com>
2019-01-21 18:14:10 -0200

    teste

--- offline.js
+++ offline.js
@@ -5,6 +5,24 @@
 
 	const staticCacheName = 'cache_calculadoras';	
 	
+	self.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise.
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
+	
     self.addEventListener("install", function(event) {
 	  console.log('WORKER: install event in progress.');
 	  event.waitUntil(
